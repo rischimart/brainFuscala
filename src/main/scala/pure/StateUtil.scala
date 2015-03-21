a66859b7b186300717ec2104ba88ab9ec863d152
Bocheng Zhou <fromsquaresone@gmail.com>
2015-03-21 15:41:49 -0400

    stste monad works

--- src/main/scala/pure/StateUtil.scala
+++ src/main/scala/pure/StateUtil.scala
@@ -30,9 +30,7 @@ object StateUtil {
   }
   
   def put[S](s : S) : State[S, Unit] = StateM(_ => ((), s))
-  
   def get[S]: State[S, S] = StateM(s => (s, s))
-  //implicit def monadify[S, A](st: => State[S, A]) = new StateMonad[S]("state") 
 
 }
 
@@ -47,21 +45,21 @@ object Main {
             then put [8,3,1]  
             else put [9,2,1]  
      */
-    type Stack = List[Int]
     
     /*
-    def stacky : State[Stack, Unit] = {
-      val program = for {
-        st <- get[Stack]
-        _ <-
-      }
-      
-    }*/
-
+    get[Stack] ⇒ ToBindOpsUnapply(get[Stack]): 
+    (implicit F0: scalaz.Unapply[scalaz.Bind,State[Stack,Stack]]) BindOps[F0.M,F0.A]
+    * 
+    */
+    type Stack = List[Int]
     def main(args : Array[String]) : Unit = {
       val program = get[Stack] >>= {st => if (st == List(1, 2, 3)) put(List(8, 3, 1))
                                         else put(List(9, 2, 1))}
-      val (_, finalstate) = program.runState(List(1, 2, 4))
+      val program2 = for {
+        st <- get[Stack]
+        _ <- if (st == List(1, 2, 3)) put(List(8, 3, 1)) else put(List(9, 3, 1))
+      } yield ()
+      val (_, finalstate) = program2.runState(List(1, 2, 4))
       print(finalstate)
     }
 } 
